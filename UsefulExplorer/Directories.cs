9df1fd6d481d1b0a2a37225de39119b0d073a5eb
Martin Jindra <jindra.martin@hotmail.com>
2019-11-11 20:22:06 +0100

    Added listing directories capabilies. (Stil WIP)

---
+++ UsefulExplorer/Directories.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace UsefulExplorer
+{
+	class Directories
+	{
+		private string path;
+
+		public Directories() {}
+
+		public Directories(string path)
+		{
+			if (path != null)
+			{
+				this.setPath(path);
+			}
+		}
+
+		public void setPath(string path)
+		{
+			this.path = path;
+		}
+
+		public string getPath()
+		{
+			return this.path;
+		}
+
+		public static void listFiles(string path)
+		{
+			short num = Directories.getHowManyPaths(path);
+			if (File.Exists(path))
+			{
+				DateTime d = File.GetLastAccessTime(path);
+				for (int i = 0; i < num; i++)
+				{
+					Console.Write("\t");
+				}
+				Console.WriteLine(Path.GetFileName(path) + " - " + d.Date +  " - " + new FileInfo(path).Length + " Bytes");
+			}
+			else if (Directory.Exists(path))
+			{
+				for (int i = 0; i < num; i++)
+				{
+					Console.Write("\t");
+				}
+				Console.WriteLine("+" + Path.GetFullPath(path));
+				foreach (string s in Directory.GetFiles(path))
+				{
+					Console.WriteLine(s);
+				}
+			}
+		}
+
+		private static short getHowManyPaths(string path)
+		{
+			short num = 0;
+			string s = Path.GetFullPath(path);
+			for (int i = 0; i < s.Length; i++)
+			{
+				if (s[i] == '\\' || s[i] == '/')
+				{
+					num++;
+				}
+			}
+			return num;
+		}
+	}
+}
--- UsefulExplorer/Program.cs
+++ UsefulExplorer/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
+/**
+ * 
+ * @author Martin Jindra
+ * @version 11.11.2019
+ */
 namespace UsefulExplorer
 {
 	class Program
 	{
+		private Dictionary<string, string> arg;
+		private Directories dir;
+
+		public Program(string[] args)
+		{
+			this.dir = new Directories();
+			this.init(args);
+		}
+
+		private void init(string[] args)
+		{
+			if (args.Length > 0)
+			{
+				this.arg = new Dictionary<string, string>();
+				for (int i = 0; i < args.Length; i++)
+				{
+					if (args[i] == "-l")
+					{
+						this.arg.Add("-l", args[i + 1]);
+					}
+				}
+				if (this.arg.ContainsKey("-l"))
+				{
+					Directories.listFiles(this.arg["-l"]);
+				}
+			}
+			else
+			{
+				Console.WriteLine("Nothing to do");
+			}
+		}
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			args = new string[2];
+			args[0] = "-l";
+			args[1] = "D:\\Handy\\Handy.tar.gz";
+			
+			new Program(args);
 		}
 	}
 }
